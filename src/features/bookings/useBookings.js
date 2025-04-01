2bf67c5e3b428b53368fde541272304e1698caf9
kakaijiro <kakaijiro@gmail.com>
2025-04-01 17:47:25 +0900

    add filtering functionality to bookings with useSearchParams and BookingTableOperations component

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,14 +1,25 @@
 import { useQuery } from "@tanstack/react-query";
 import { getBookings } from "../../services/apiBookings";
+import { useSearchParams } from "react-router-dom";
 
 export function useBookings() {
+  const [searchParams] = useSearchParams();
+
+  // filter
+  const filterValue = searchParams.get("status");
+  const filter =
+    !filterValue || filterValue === "all"
+      ? null
+      : { field: "status", value: filterValue };
+  // : { field: "status", value: filterValue, method: "gte" }; // this is an example of using a different method than the default "eq" for filtering. Furthermore, for multiple fields, use an array of objects
+
   const {
     isLoading,
     data: bookings,
     error,
   } = useQuery({
-    queryKey: ["bookings"],
-    queryFn: getBookings,
+    queryKey: ["bookings", filter], // this works like useEffect's dependency array where everytime value updates, the query will be re-fetched
+    queryFn: () => getBookings({ filter }),
   });
   // queryFn must return a Promise.
 
